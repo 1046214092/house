70be843eb3b024c9b00ef1d0a502e3a178e8c6e0
Administrator <1046214092@qq.com>
2017-07-25 14:41:28 +0800

    create project

--- house-web/src/main/java/com/zcq/house/web/TestController.java
+++ house-web/src/main/java/com/zcq/house/web/TestController.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,7 +19,7 @@ import java.util.Objects;
 /**
  * Created by Administrator on 2017/7/24.
  */
-@Controller
+@RestController
 @RequestMapping("a")
 public class TestController {
     public Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -25,13 +27,16 @@ public class TestController {
     @Autowired
     private TestService testService;
 
-    @RequestMapping("a")
+    @RequestMapping("a/{id}")
     @ResponseBody
-    public List<Test> a(){
+    public List<Test> a(@PathVariable String id){
+        logger.error(id);
         List<Test> list = testService.getList();
         logger.error("size:{},{}",list.size(),list);
         return  list;
     }
+
+
 
     public TestService getTestService() {
         return testService;
