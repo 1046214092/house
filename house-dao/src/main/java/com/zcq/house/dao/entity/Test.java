7e8b786b9aed99e675f842098d5becc97e10e973
Administrator <1046214092@qq.com>
2017-07-25 11:01:39 +0800

    create project

---
+++ house-dao/src/main/java/com/zcq/house/dao/entity/Test.java
@@ -0,0 +1,32 @@
+package com.zcq.house.dao.entity;
+
+public class Test {
+    private String id;
+
+    private Integer name;
+
+    public Test(String id, Integer name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Test() {
+        super();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(Integer name) {
+        this.name = name;
+    }
+}
